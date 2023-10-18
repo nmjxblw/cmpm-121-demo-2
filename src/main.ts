b53eac67cd8bd24ed651233a09ea50a54b1ef88a
nmjxblw <zchen386@ucsc.edu>
2023-10-17 20:17:32 -0700

    setp2 complete

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,13 @@ header.innerHTML = gameName;
 app.append(header);
 
 //step1
+//setp2
 const canvas = document.createElement("canvas");
 const canvasScale = 256;
 canvas.width = canvasScale;
 canvas.height = canvasScale;
 canvas.style.border = "thin solid black";
+canvas.style.borderRadius = "20px";
 canvas.style.filter = "drop-shadow(0 0 1px crimson)";
 app.append(document.createElement("br"));
 document.body.append(canvas);
@@ -25,14 +27,18 @@ const ctx = canvas.getContext("2d");
 ctx!.fillStyle = "white";
 const cursor = { active: false, x: 0, y: 0 };
 
-//const paths = [];
+//const paths: any[] = [];
+
+//let currentIndex = paths.length - 1;
 
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+  //currentIndex++;
 });
 
+//let path: any[] = [];
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
     ctx!.beginPath();
@@ -41,11 +47,15 @@ canvas.addEventListener("mousemove", (e) => {
     ctx!.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    //path.push({ x: cursor.x, y: cursor.y });
   }
 });
 
 canvas.addEventListener("mouseup", () => {
+  //paths.push(path);
+  //path = [];
   cursor.active = false;
+  //console.log(paths);
 });
 
 const clearButton = document.createElement("button");
